a5e38ec0fffea0bca325bde60b704c39c6654983
pom <singh94564@gmail.com>
2025-11-20 14:46:11 -0800

    Initial push

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    float = {
+      padding = 5,
+      border = "rounded",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  -- Open oil normally
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Oil" }),
+  -- Toggle Oil Float
+  vim.keymap.set("n", "<leader>o", function()
+    require("oil").toggle_float()
+  end, { desc = "Toggle [O]il" }),
+}
