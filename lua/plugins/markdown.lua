cc2918b372babf8b6e9dea4fadf2fc4a2b90cf32
pom <singh94564@gmail.com>
2025-11-27 00:30:33 -0800

    Added nvim notes

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,77 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    render_modes = true,
+    code = {
+      sign = true,
+      -- width = "block",
+      -- right_pad = 1,
+    },
+    heading = {
+      enabled = true,
+      render_modes = false,
+      atx = true,
+      setext = true,
+      sign = true,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      position = "overlay",
+      signs = { "󰫎 " },
+      width = "full",
+      left_margin = 0,
+      left_pad = 0,
+      right_pad = 0,
+      min_width = 0,
+      border = false,
+      border_virtual = false,
+      border_prefix = false,
+      above = "▄",
+      below = "▀",
+      backgrounds = {
+        "RenderMarkdownH1Bg",
+        "RenderMarkdownH2Bg",
+        "RenderMarkdownH3Bg",
+        "RenderMarkdownH4Bg",
+        "RenderMarkdownH5Bg",
+        "RenderMarkdownH6Bg",
+      },
+      foregrounds = {
+        "RenderMarkdownH1",
+        "RenderMarkdownH2",
+        "RenderMarkdownH3",
+        "RenderMarkdownH4",
+        "RenderMarkdownH5",
+        "RenderMarkdownH6",
+      },
+      custom = {},
+    },
+    checkbox = {
+      enabled = true,
+      render_modes = false,
+      bullet = false,
+      left_pad = 0,
+      right_pad = 1,
+      unchecked = {
+        icon = "󰄱 ",
+        highlight = "RenderMarkdownUnchecked",
+        scope_highlight = nil,
+      },
+      checked = {
+        icon = "󰱒 ",
+        highlight = "RenderMarkdownChecked",
+        scope_highlight = nil,
+      },
+      custom = {
+        todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+      },
+      scope_priority = nil,
+    },
+  },
+  config = function(_, opts)
+    require("render-markdown").setup(opts)
+    Snacks.toggle({
+      name = "Render Markdown",
+      get = require("render-markdown").get,
+      set = require("render-markdown").set,
+    }):map("<leader>um")
+  end,
+}
