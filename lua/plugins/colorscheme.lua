eccfcbe91697f59b33b7ef97b756c38295267569
pom <singh94564@gmail.com>
2025-11-27 13:03:29 -0800

    changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,20 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("onedark").setup({
-      style = "darker",
-    })
-    require("onedark").load()
-  end,
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
+    --   -- require("onedark").setup({
+    --   --   style = "darker",
+    --   -- })
+    --   require("onedark").load()
+    -- end,
+
+    -- vim.cmd("colorscheme onedark"),
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vaporwave",
+    },
+  },
 }
